81814c96bffd47ae9e6b860f132547c61ca707b6
VL <lusakvm@gmail.com>
2023-09-06 18:15:01 +0300

    Added tasks

--- src/main/java/tasks/kata8/ColorGhost.java
+++ src/main/java/tasks/kata8/ColorGhost.java
@@ -5,7 +5,7 @@ import java.util.Random;
 //Create a class Ghost
 //Ghost objects are instantiated without any arguments.
 //Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
-public interface ColorGhost {
+public class ColorGhost {
     class Ghost {
         String getColor() {
             return switch (new Random().nextInt(4)) {
